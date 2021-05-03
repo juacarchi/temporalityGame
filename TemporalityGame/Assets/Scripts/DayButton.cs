54331934960389d4f5abd8f603568b2c2063e06a
juacarchi <cardenasfanoce@gmail.com>
2021-05-03 11:05:18 +0200

    GameplayNoria(Actualizacion)
    Actualizacion escena de noria. Creacion de botones

---
+++ TemporalityGame/Assets/Scripts/DayButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayButton : MonoBehaviour
+{
+    public string dayName;
+    public string tagButton;
+    public Button buttonDay;
+    public BoxCollider2D box2DButton;
+
+    public DayButton(string dayName, string tagButton, Button buttonDay, BoxCollider2D box2DButton)
+    {
+        this.dayName = dayName;
+        this.tagButton = tagButton;
+        this.buttonDay = buttonDay;
+        this.box2DButton = box2DButton;
+    }
+
+}
