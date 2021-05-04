2e55b789955e554de3197e7bc6bd879996243a16
juacarchi <cardenasfanoce@gmail.com>
2021-05-04 12:48:03 +0200

    DragDropTrain
    InProgress

---
+++ TemporalityGame/Assets/Scripts/DragDropTrain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DragDropTrain : MonoBehaviour, IDragHandler, IDropHandler
+{
+    Rigidbody2D rb2D;
+    BoxCollider2D bx;
+
+    public float zValue = 1;
+
+    private void Awake()
+    {
+        
+        rb2D = GetComponent<Rigidbody2D>();
+        bx = GetComponent<BoxCollider2D>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Button buttonPressed = this.GetComponent<Button>();
+        buttonPressed.enabled = false;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = zValue;
+        Vector3 point = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = point;
+        rb2D.simulated = false;
+        rb2D.isKinematic = false;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        rb2D.simulated = true;
+        Debug.Log("Suelta");
+        Button buttonPressed = this.GetComponent<Button>();
+        buttonPressed.enabled = true;
+    }
+}
--- TemporalityGame/Assets/Scripts/GameplayTrainManager.cs
+++ TemporalityGame/Assets/Scripts/GameplayTrainManager.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayTrainManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Text> textList;
+    public List<Button> buttonList;
+    public List<Transform> posButtons;
 
-    // Update is called once per frame
-    void Update()
+    public List<Meses> mesesList;
+    private void Start()
     {
-        
+        RandomMonth();
+    }
+    public void RandomMonth()
+    {
+        int numberMonth = Random.Range(1, mesesList.Count - 1);
+        Meses mesPreview = mesesList[numberMonth - 1];
+        Meses mesChosen = mesesList[numberMonth];
+        Meses mesForward = mesesList[numberMonth + 1];
+
+        textList[0].text = mesPreview.monthName;
+        textList[0].enabled = false;
+        textList[1].text = mesChosen.monthName;
+        textList[2].text = mesForward.monthName;
+        textList[2].enabled = false;
+
+        Text textButton1 = buttonList[0].GetComponentInChildren<Text>();
+        Text textButton2 = buttonList[1].GetComponentInChildren<Text>();
+
+        textButton1.text = mesPreview.monthName;
+        textButton2.text = mesForward.monthName;
+
+        //List<Transform> posButtonsProvisional = new List<Transform>(posButtons);
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            int numberPos = Random.Range(0, posButtons.Count);
+            buttonList[i].transform.position = posButtons[numberPos].transform.position;
+            posButtons.RemoveAt(numberPos);
+        }
     }
 }
