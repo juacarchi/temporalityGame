09f3f76ad25c0c9fc4575610e737c63b7e57300b
juacarchi <cardenasfanoce@gmail.com>
2021-05-05 11:15:36 +0200

    CanvasVictory_Noria
    In Progress

--- TemporalityGame/Assets/Scripts/GameplayTrainManager.cs
+++ TemporalityGame/Assets/Scripts/GameplayTrainManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GameplayTrainManager : MonoBehaviour
 {
     public static GameplayTrainManager instance;
+    public Canvas canvasVictory;
     public Animator animTrain;
     public List<Text> textList;
     public List<Button> buttonList;
@@ -26,7 +27,7 @@ public class GameplayTrainManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
-        
+        canvasVictory.enabled = false;
     }
     private void Start()
     {
@@ -112,7 +113,10 @@ public class GameplayTrainManager : MonoBehaviour
         ManagerScene.instance.SetNumberSceneToChange(0);
         TransitionManager.instance.AnimateTransition();
     }
-
+    public void Victory()
+    {
+        canvasVictory.enabled = true;
+    }
 
 
 }
---
+++ TemporalityGame/Assets/Scripts/TrainWin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainWin : MonoBehaviour
+{
+ public void Victory()
+    {
+        GameplayTrainManager.instance.Victory();
+    }
+}
