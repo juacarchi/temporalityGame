a8ffa4f545320652cef2e9366e99f9ee429b1050
juacarchi <cardenasfanoce@gmail.com>
2021-05-26 13:59:31 +0200

    ActualizacionSerializacion

--- TemporalityGame/Assets/Scripts/GameplayManagerSerializacion.cs
+++ TemporalityGame/Assets/Scripts/GameplayManagerSerializacion.cs
@@ -1,16 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayManagerSerializacion : MonoBehaviour
 {
+    public List<Serie> listaSeries;
     public GameObject canvasVictory;
-    int aciertosToComplete = 3; //ANTES, AHORA Y DESPUÉS
+    public Image imageAntes;
+    public Image imageAhora;
+    public Image imageDespues;
+    
+   
+    public List<Image> imagesCompletes;
+    
+
+    int aciertosToComplete = 2; //ANTES, AHORA Y DESPUÉS
     private void Awake()
     {
         canvasVictory.SetActive(false);
     }
+    private void Start()
+    {
+        RandomSerie();
+    }
+    public void RandomSerie()
+    {
+        
+        int randomCorrect = Random.Range(0, imagesCompletes.Count);
+        int randomNumber = Random.Range(0, listaSeries.Count);
+        Serie newSerie = listaSeries[randomNumber];
+        Debug.Log(newSerie.name);
+        imagesCompletes[randomCorrect].sprite = newSerie.GetSprite(randomCorrect);
+        imagesCompletes[randomCorrect].color = Color.white;
 
+        if(randomCorrect == 0)
+        {
+            imageAntes.enabled = false;
+            imageAhora.sprite = newSerie.GetSprite(1);
+            imageDespues.sprite = newSerie.GetSprite(2);
+        }
+        else if(randomCorrect == 1)
+        {
+            imageAntes.sprite = newSerie.GetSprite(0);
+            imageAhora.enabled = false;
+            imageDespues.sprite = newSerie.GetSprite(2);
+        }
+        else if (randomCorrect == 2)
+        {
+            imageAntes.sprite = newSerie.GetSprite(0);
+            imageAhora.sprite = newSerie.GetSprite(1);
+            imageDespues.enabled = false;
+        }
+        
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ TemporalityGame/Assets/Scripts/Serie.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serie : MonoBehaviour
+{
+    public Sprite spriteAntes;
+    public Sprite spriteAhora;
+    public Sprite spriteDespues;
+
+    public Serie(Sprite spriteAntes, Sprite spriteAhora, Sprite spriteDespues)
+    {
+        this.spriteAntes = spriteAntes;
+        this.spriteAhora = spriteAhora;
+        this.spriteDespues = spriteDespues;
+    }
+    public Sprite GetSprite(int i)
+    {
+        if (i == 0)
+        {
+            return spriteAntes;
+        }
+        else if (i == 1)
+        {
+            return spriteAhora;
+        }
+        else if (i == 2)
+        {
+            return spriteDespues;
+        }
+        else return null;
+    }
+   
+}
